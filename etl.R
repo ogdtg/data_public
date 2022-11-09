c6bc0bbad621bcf1f42276504b8e30667a9bf6db
ogd1 <ogd@tg.ch>
2022-11-09 14:46:34 +0100

    minor changes

--- etl.R
+++ etl.R
@@ -1,12 +1,17 @@
 library(readxl)
 library(dplyr)
 
-select_file_old <-tryCatch({
-  select_file_old = readRDS("Cache/select_file.rds")
-  return(select_file_old)
-},error = function(cond) {
-  return(NA)
-})
+
+get_cache_file <- function(){
+  tryCatch({
+    select_file_old = readRDS("Cache/select_file.rds")
+    return(select_file_old)
+  },error = function(cond) {
+    return(NA)
+  })
+}
+
+select_file_old <- get_cache_file()
 
 path = "Input/"
 
@@ -26,6 +31,13 @@ select_file <- info_list %>%
 
 if (is.na(select_file_old)) {
   saveRDS(select_file,"Cache/select_file.rds")
+  result <- read_excel(select_file)
+  write.table(result,file="Output/output_data.csv",
+              fileEncoding = "UTF-8",
+              quote = T,
+              dec = ".",
+              sep = ",")
+  message("CSV saved in Output")
 } else {
   if (select_file_old==select_file){
     message("select file is still the same")
